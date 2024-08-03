23b5c93e3661602b2df5270e379353d0cf1a6c06
KevinFi92 <168017579+KevinFi92@users.noreply.github.com>
2024-08-03 16:21:24 +0200

    finished
    responsive design missing

--- script.js
+++ script.js
@@ -174,9 +174,11 @@ let books = [
   }
 ]
 function render_all() {
+  loadFromLocalStorage()
   render_content();
   renderLiked();
-  renderComments();
+
+  
 }
 
 
@@ -189,17 +191,22 @@ function render_content() {
       `<div class="items"><div class="bookTitel">${books[indexBooks].name}</div>
 <div class="img"><img class="bookImg" src="./img/book.png" alt="book"></div>
 <div class="midsection"><div class="price">${books[indexBooks].price} €</div>
-<div class="likes"><div><img class="likeimg" onclick="likeBook(${indexBooks})" src="" id="heart${indexBooks}"  alt="heart"></div><div>${books[indexBooks].likes}</div></div></div>
+<div class="likes"><div><img class="likeimg" onclick="likeBook(${indexBooks})" src="" id="heart${indexBooks}" alt="heart"></div><div>${books[indexBooks].likes}</div></div></div>
 <div class="info">
 <div class="author">Autor: ${books[indexBooks].author}</div>
 <div class="year">Erschienen: ${books[indexBooks].publishedYear}</div>
 <div class="genre">Genre: ${books[indexBooks].genre}</div>
 </div>
-<div id="comments">
-</div>`;
-  }
+<div class="cmtDiv"><h3>Kommentare</h3>
+<div id="comments${indexBooks}" class="cmtDiv">
+${book.comments.map(comment => `<div><b>${comment.name}</b>: ${comment.comment}</div>`).join('')}
+</div>
+<br>
+<input id="commentInput${indexBooks}"type="text" placeholder="kommentar"><button onclick= addComment(${indexBooks})>hinzufügen</button>
+</div>`
 
-}
+  };
+  }
 
 function renderLiked() {
 
@@ -214,27 +221,6 @@ function renderLiked() {
 }
 
 
-function renderComments() {
-
-  document.getElementById('comments').innerHTML = "";
-  for (let indexBooks = 0; indexBooks < books.length; indexBooks++) {
-    let comment = books[indexBooks].comments[indexBooks];
-    console.log(comment);
-
-    if (comment === "") {
-      document.getElementById('comments').innerHTML += `<div id="comment_name${indexBooks}"></div>
-      <div id="comment_text${indexBooks}"></div>`;
-    }
-else {
-    document.getElementById('comments').innerHTML += `<div id="comment_name">${books[indexBooks].comments[indexBooks].name}</div>
-    <div id="comment_text">${books[indexBooks].comments[indexBooks].comment}</div>`
-  }
-
-
-}
-}
-
-
 function likeBook(indexBooks) {
 
   if (books[indexBooks].liked === true) {
@@ -244,9 +230,24 @@ function likeBook(indexBooks) {
     books[indexBooks].liked = true;
     books[indexBooks].likes++;
   }
+  saveToLocalStorage();
   render_all();
 };
 
 
+function addComment(indexBooks){
+  let newComment = document.getElementById(`commentInput${indexBooks}`).value;
+document.getElementById(`comments${indexBooks}`).innerHTML += `<b>User</b> : ${newComment}`;
+saveToLocalStorage();
+}
 
 
+function saveToLocalStorage(){
+  localStorage.setItem("savedBooks", JSON.stringify(books));
+}
+
+
+function loadFromLocalStorage(){
+let loadedBooks = localStorage.getItem('savedBooks');
+books = JSON.parse(loadedBooks);
+}
